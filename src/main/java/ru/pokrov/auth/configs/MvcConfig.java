036bebda00d41e896b1cb51bc37a976f691557af
pokrov <al.n.pokrovsky@gmail.com>
2020-12-07 01:05:24 +0300

    angular static files support

--- src/main/java/ru/pokrov/auth/configs/MvcConfig.java
+++ src/main/java/ru/pokrov/auth/configs/MvcConfig.java
@@ -10,30 +10,24 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
 
-//@Configuration
-//public class MvcConfig implements WebMvcConfigurer {
-//
-////    public void addViewControllers(ViewControllerRegistry registry) {
-////        registry.addViewController("/home").setViewName("home");
-////        registry.addViewController("/").setViewName("home");
-////        registry.addViewController("/hello").setViewName("hello");
-////        registry.addViewController("/login").setViewName("login");
-////    }
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/**")
-//                .addResourceLocations("classpath:/static/")
-//                .resourceChain(true)
-//                .addResolver(new PathResourceResolver() {
-//                    @Override
-//                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
-//                        Resource requestedResource = location.createRelative(resourcePath);
-//
-//                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-//                                : new ClassPathResource("/static/index.html");
-//                    }
-//                });
-//    }
-//
-//}
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**/*")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                                                   Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable()
+                                ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
+
+}
