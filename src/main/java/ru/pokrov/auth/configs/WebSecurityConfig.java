e11b265070f96a90c9f826d7661b7787b2bd8f06
pokrov <al.n.pokrovsky@gmail.com>
2020-12-08 18:58:07 +0300

    auth jwt token

--- src/main/java/ru/pokrov/auth/configs/WebSecurityConfig.java
+++ src/main/java/ru/pokrov/auth/configs/WebSecurityConfig.java
@@ -4,38 +4,69 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import ru.pokrov.auth.configs.jwt.JwtRequestFilter;
 import ru.pokrov.auth.services.UserService;
 
 @Configuration
 @EnableWebMvc
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
     UserService userService;
 
+    @Autowired
+    AuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
-            .and()
+        http//.csrf().disable()
                 .authorizeRequests()
-                .antMatchers( "/api/signup").permitAll()
+                // allow any user to signup and signin
+                .antMatchers("/api/signup", "/api/signin").permitAll()
+                // allow angular to load from /app
                 .antMatchers(HttpMethod.GET, "/app/**").permitAll()
+                // OPTIONS call are made by Angular application to Spring Boot application
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                // any other request need auth
                 .anyRequest().authenticated()
             .and()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and()
+                // It rejects every unauthenticated request and send error code 401
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .and()
+                // make sure we use stateless session;
+                // session won't be used to store user's state.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                // Add a filter to validate the tokens with every request
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
